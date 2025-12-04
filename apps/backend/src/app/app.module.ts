caee3b6af49b59316e557ca883954d2207d89da7
Jhoiner Martinez <153531218+Jhoiner243@users.noreply.github.com>
2025-12-04 14:59:29 -0500

    feat: Add initial products module setup with service test and comprehensive development scripts documentation.

--- apps/backend/src/app/app.module.ts
+++ apps/backend/src/app/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ProductsController } from './products/products.controller';
 import { ProductsModule } from './products/products.module';
 import { ProductsService } from './products/products.service';
 
 @Module({
   imports: [ProductsModule],
-  controllers: [AppController, ProductsController],
-  providers: [AppService, ProductsService],
+  controllers: [ProductsController],
+  providers: [ProductsService],
 })
 export class AppModule {}
